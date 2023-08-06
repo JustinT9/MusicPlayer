6409adfc32e8701f241d7a5744941cb3f3ab422f
Justin Tran <jus81818@yahoo.com>
2023-08-06 13:59:35 -0400

    finished backend

---
+++ src/LoginPlayer.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react'; 
+import './LoginPlayer.css'; 
+
+function LoginPlayer() {
+    return (
+        <>
+        qw
+        </>
+    )
+}
+
+export default LoginPlayer; 

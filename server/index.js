a5b1293805151d23ea1d739d332db9bc29140eff
JustinT9 <jus81818@yahoo.com>
2023-08-07 16:37:30 -0400

    finished

--- server/index.js
+++ server/index.js
@@ -71,6 +71,27 @@ app.get('/auth/callback', (req, res) => {
     })
 });
 
+app.get('/refresh_token', (req, res) => {
+    var refresh_token = req.query.refresh_token; 
+    var authOptions = {
+        url: 'https://accounts.spotify.com/api/token', 
+        headers: {
+            'Authorization': 'Basic ' + (new Buffer.from(client_id + ":" + client_secret).toString('base64')) }, 
+        form: {
+            grant_type: 'refresh_token', 
+            refresh_token: refresh_token
+        }, 
+        json: true 
+    }
+
+    request.post(authOptions, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+            var access_token = body.access_token; 
+            res.send({ 'access_token': access_token })
+        }
+    })
+})
+
 // returns access token from the request as json to the /auth/token endpoint 
 // which will allow us to start the music player and make API calls and retrieve important data 
 app.get('/auth/token', (req, res) => {
